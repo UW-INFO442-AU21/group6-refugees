50895b551866a5614c4a2d131b401ca92a612c0b
chance-landis <maolandis@gmail.com>
2021-11-29 15:30:39 -0800

    org css

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,7 +8,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Group />
+    <Group/>
+    <Orgs />
   </React.StrictMode>,
   document.getElementById('root')
 );
