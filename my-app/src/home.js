02a33bbfae7ccc2843950b9b9583863f08c00d98
Padma Gundapaneni <padmag@uw.edu>
2021-12-07 05:30:20 -0800

    add new header img and add cards for team section

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -1,4 +1,13 @@
 import './home.css';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import angelina from './picture_assets/headshots/angelina.JPG'
+import chance from './picture_assets/headshots/chance.jpg'
+import ethan from './picture_assets/headshots/ethan.jpg'
+import kaamna from './picture_assets/headshots/kaamna.jpg'
+import padma from './picture_assets/headshots/padma.jpg'
+import migrants from './picture_assets/migrants.jpg'
 
 function Home() {
     return(
@@ -6,7 +15,7 @@ function Home() {
             <header>
                 <h1 className="WelcomeHeader">WELCOME!</h1>
                 <div class="row">
-                    <div class="col-lg-8 col-md-6">
+                    <div class="col-lg-6 col-md-6">
                         <p className="WelcomeDesc">We are RAUWR: Refugee Asylee University of Washington
                             Resource. A service that assists the general UW population
                             and all of its staff and students in learning more about the
@@ -14,8 +23,8 @@ function Home() {
                             how we can help those seeking Asylum or Refuge in our
                             community with services or needs that they have.</p>
                     </div>
-                    <div class="col-12 col-lg-4 col-md-6">
-                        <img src="https://raw.githubusercontent.com/UW-INFO442-AU21/group6-refugees/clform/my-app/src/picture_assets/image%201.png" alt="" class="img-responsive float-right mr-3"/>
+                    <div class="col-12 col-lg-6 col-md-6">
+                        <img className = "migrants" src={migrants} alt="Migrants" class="img-responsive float-right mr-3"/>
                     </div>
                 </div>
             </header>
@@ -23,14 +32,72 @@ function Home() {
             <p className="Desc"> As the team behind RAUWR, we seek to provide communities
                 here at UW with the information neccessary to come together
                 and serve others in our communities that are in a vulnerable
-                position. An individual applyinf for Asylum or Refuge is seeking
+                position. An individual applying for Asylum or Refuge is seeking
                 protection due to well-founded fear or reason of persecution.
                 Due to these reasons, they need support building from the ground
                 up and we all have a part in strengthening our communities with
                 mutual aid and support. RAUWR centralizes information and service
                 for you to assist the Refugees and Asylees in our area.</p>
             <h2 className="TeamHeader">MEET THE TEAM</h2>
-            
+            <Row xs={1} md={2} lg='auto' className="g-4">
+                <Col>
+                    <Card className='cardbg mb-2' style={{ width: '18rem'}}>
+                    <Card.Img classname = 'cardimg' variant='top' src=""/>
+                        <div className='textalign'>
+                            <Card.Body>
+                                <Card.Title className='cardtitle'>Kaamna Rishi</Card.Title>
+                                <Card.Subtitle className="mb-2">Product Manager</Card.Subtitle>
+                            </Card.Body>
+                        </div>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className='cardbg mb-2' style={{ width: '18rem' }}>
+                    <Card.Img classname = 'cardimg' variant='top' src=""/>
+                        <div className='textalign'>
+                            <Card.Body>
+                                <Card.Title className='cardtitle'>Angelina Lum</Card.Title>
+                                <Card.Subtitle className="mb-2">UX Researcher + UX Designer</Card.Subtitle>
+                            </Card.Body>
+                        </div>    
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className='cardbg mb-2' style={{ width: '18rem' }}>
+                    <Card.Img classname = 'cardimg' variant='top' src=""/>
+                        <div className='textalign'>
+                            <Card.Body>
+                                <Card.Title className='cardtitle'>Ethan Kuhn</Card.Title>
+                                <Card.Subtitle className="mb-2">UX Researcher + UX Designer</Card.Subtitle>
+                            </Card.Body>
+                        </div>    
+                    </Card>
+                </Col>
+            </Row>
+            <Row xs={1} md={2} lg='auto' className="g-4">
+                <Col>
+                    <Card className='cardbg mb-2' style={{ width: '18rem'}}>
+                    <Card.Img classname = 'cardimg' variant='top' src=""/>
+                        <div className='textalign'>
+                            <Card.Body>
+                                <Card.Title className='cardtitle'>Padma Gundapaneni</Card.Title>
+                                <Card.Subtitle className="mb-2">Developer</Card.Subtitle>
+                            </Card.Body>
+                        </div>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card className='cardbg mb-2' style={{ width: '18rem' }}>
+                    <Card.Img classname = 'cardimg' variant='top' src=""/>
+                        <div className='textalign'>
+                            <Card.Body>
+                                <Card.Title className='cardtitle'>Chance Landis</Card.Title>
+                                <Card.Subtitle className="mb-2">Developer</Card.Subtitle>
+                            </Card.Body>
+                        </div>    
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
